5a5dd2a98de6c5af688f88176c50ced66cee77aa
Sander Goetzee <sander.goetzee@hva.nl>
2018-11-20 15:43:10 +0100

    Alarm done

---
+++ AGD GJ2/Assets/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public FloatVar timer;
+    public Color color;
+    private Light light;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer._floatVar <= 5f)
+        {
+            light.color = color;
+        }
+	}
+}
