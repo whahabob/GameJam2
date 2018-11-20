60af0218b3054c6545c6ed88d2b3f1f8001db3aa
RikLangeveld <Rik_langeveld@hotmail.com>
2018-11-20 14:39:04 +0100

    rgsofigiuofdsg

---
+++ AGD GJ2/Assets/OnTriggerSucces.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerSucces : MonoBehaviour
+{
+    [SerializeField]
+    private Transform door;
+
+    [SerializeField]
+    private ActiveDoors activeDoors;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SuccesEffect();
+            activeDoors.openDoors.Add(door.gameObject);
+            //activeDoors.resetDoors();
+        }
+    }
+
+    private void SuccesEffect()
+    {
+
+    }
+}
