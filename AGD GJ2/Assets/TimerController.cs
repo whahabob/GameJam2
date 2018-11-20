f5b71e6c0b66d2b63b8c3a8cb122f801c13f6be8
RikLangeveld <Rik_langeveld@hotmail.com>
2018-11-20 14:54:52 +0100

    fixed shit

--- AGD GJ2/Assets/TimerController.cs
+++ AGD GJ2/Assets/TimerController.cs
@@ -4,41 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimerController : MonoBehaviour {
-
-    public static TimerController instance;
-    public float timer;
-    GameObject[] timerObjects;
-
-    void Awake()
-    {
-        instance = this;
-        timerObjects = GameObject.FindGameObjectsWithTag("timerObject");
-    }
+public class TimerController : MonoBehaviour
+{
+    public FloatVar timer;
 
     // Use this for initialization
-    void Start () {
-        timer = 10f;
+    void Start ()
+    {
+        timer._floatVar = 10f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         Timer();
     }
 
     private void Timer()
     {
-        if (timer >= 0)
+        if (timer._floatVar >= 0)
         {
-            timer -= Time.deltaTime;
-            foreach (GameObject o in timerObjects)
-            {
-                o.GetComponentInChildren<Text>().text = Math.Round(timer).ToString();
-            }
-        }
-        else
-        {
-            // Player Died
+            timer._floatVar -= Time.deltaTime;
         }
     }
 }
