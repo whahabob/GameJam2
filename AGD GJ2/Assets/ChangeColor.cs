632fec4ca35a4e495d59a8f8cb1987ec2f30f29c
Sander Goetzee <sander.goetzee@hva.nl>
2018-11-21 09:17:54 +0100

    Fixed color not changing back

--- AGD GJ2/Assets/ChangeColor.cs
+++ AGD GJ2/Assets/ChangeColor.cs
@@ -6,12 +6,14 @@ public class ChangeColor : MonoBehaviour {
 
     public FloatVar timer;
     public Color color;
+    private Color originalColor;
     private Light light;
 
 	// Use this for initialization
 	void Start () {
         light = GetComponent<Light>();
-	}
+        originalColor = light.color;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -19,6 +21,9 @@ public class ChangeColor : MonoBehaviour {
 		if(timer._floatVar <= 5f)
         {
             light.color = color;
+        } else if(timer._floatVar > 5f)
+        {
+            light.color = originalColor;
         }
 	}
 }
